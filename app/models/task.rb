5ba1abe0666e37438ce5e398f12ffc9ede0e949e
Aryanand Arnav <56192760+aryanandarnav13@users.noreply.github.com>
2022-09-13 20:07:15 +0530

    Added a user model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,12 +2,36 @@
 
 class Task < ApplicationRecord
   MAX_TITLE_LENGTH = 125
+  # before_validation :set_title
+  before_validation :set_title, if: :title_not_present
+  before_validation :print_set_title
   validates :title, presence: true, length: { maximum: 125 }
   validates :slug, uniqueness: true
   validate :slug_not_changed
   before_create :set_slug
+  after_save :change_title
 
   private
+
+    def title_not_present
+      self.title.blank?
+     end
+
+    def title_present
+      self.title.present?
+    end
+
+    def set_title
+      self.title = "Pay electricity bill"
+    end
+
+    def change_title
+      self.title = "Pay electricity & TV bill"
+    end
+
+    def print_set_title
+      puts self.title
+    end
 
     def set_slug
       title_slug = title.parameterize
